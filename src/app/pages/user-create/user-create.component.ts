ae3b8a39e7b702b07667bd65628a98054aab7b5c
Tamta <tamtakopadz16@gmail.com>
2024-04-09 11:25:26 +0400

    edit code

--- src/app/pages/user-create/user-create.component.ts
+++ src/app/pages/user-create/user-create.component.ts
@@ -1,81 +1,100 @@
-import { Component, OnDestroy, OnInit, inject } from '@angular/core';
-import { ActivatedRoute, Router, RouterLink, RouterOutlet } from '@angular/router';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms'
-import {MatButtonModule} from '@angular/material/button';
-import {MatCalendarCellClassFunction, MatDatepickerModule} from '@angular/material/datepicker';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {  AsyncPipe, DatePipe, NgStyle } from '@angular/common';
+import { Component, OnDestroy, inject } from '@angular/core';
+import {
+  ActivatedRoute,
+  Router,
+  RouterLink,
+  RouterOutlet,
+} from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormArray,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AsyncPipe, DatePipe, NgStyle } from '@angular/common';
 import { UserService } from '../../service/user.service';
 import { User } from '../../models/user.interface';
-import { Subject, map, switchMap, takeUntil, tap } from 'rxjs';
+import { Subject, map, of, switchMap, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-user-create',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe, DatePipe,RouterLink,DatePipe, FormsModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatButtonModule,MatIconModule,MatTooltipModule,NgStyle,ReactiveFormsModule],
-
+  imports: [
+    RouterOutlet,
+    AsyncPipe,
+    DatePipe,
+    RouterLink,
+    DatePipe,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    NgStyle,
+    ReactiveFormsModule,
+  ],
   templateUrl: './user-create.component.html',
-  styleUrl: './user-create.component.scss'
+  styleUrl: './user-create.component.scss',
 })
-export class UserCreateComponent implements OnDestroy{
-service = inject(UserService)
-router = inject(Router)
-activatedRoute=inject(ActivatedRoute)
+export class UserCreateComponent implements OnDestroy {
+  service = inject(UserService);
+  router = inject(Router);
+  activatedRoute = inject(ActivatedRoute);
 
-  // dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
-  //   if (view === 'month') {
-  //     const date = cellDate.getDate();
-  //     return date === 1 || date === 20 ? 'example-custom-date-class' : '';
-  //   }
-  //   return '';
-  // };
   form = new FormGroup({
-    id:new FormControl(''),
-    firstName: new FormControl<string>('',Validators.required),
-    lastName: new FormControl('',Validators.required),
-    birthDate: new FormControl('',Validators.required),
-    phone: new FormControl('',[Validators.required,Validators.minLength(8)]),
+    id: new FormControl(''),
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    birthDate: new FormControl('', Validators.required),
+    phone: new FormControl('', [Validators.required, Validators.minLength(8)]),
     address: new FormGroup({
-      city: new FormControl('',Validators.required),
-      address: new FormControl('',Validators.required),
+      city: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
     }),
     skills: new FormArray([
       new FormGroup({
-        skill: new FormControl('',Validators.required),
+        skill: new FormControl('', Validators.required),
       }),
     ]),
-     workExperiance :new FormArray([
+    workExperiance: new FormArray([
       new FormGroup({
-        workplace:new FormControl('',Validators.required),
-        position:new FormControl('',Validators.required),
-        period:new FormControl('',Validators.required)
-      })
-     ])
+        workplace: new FormControl('', Validators.required),
+        position: new FormControl('', Validators.required),
+        period: new FormControl('', Validators.required),
+      }),
+    ]),
   });
 
-  birthdate = new  Date()
-  subj$=new Subject()
-  user$=this.activatedRoute.params
-  .pipe(
-    map(params=> params ['id']),
-    switchMap((id)=>this.service.getUser(id)
-    .pipe(
-      tap(user=>{
-        if(!user) return
-        this.form.patchValue(user)
-      })
-    )
-  )
-  )
+  subj$ = new Subject();
+  user$ = this.activatedRoute.params.pipe(
+    map((params) => params['id']),
+    switchMap((id) => {
+      if (id) {
+        return this.service.getUser(id).pipe(
+          tap((user: any) => {
+            this.form.patchValue(user);
+          })
+        );
+      }
+      return of();
+    })
+  );
 
   get skills() {
     return this.form.get('skills') as FormArray;
   }
 
-  get workExperiance(){
+  get workExperiance() {
     return this.form.get('workExperiance') as FormArray;
   }
 
@@ -86,25 +105,25 @@ activatedRoute=inject(ActivatedRoute)
     this.skills.push(newControl);
   }
 
-  addexperiance(){
-    const newexperiance= new FormGroup({
-      workplace:new FormControl(''),
-      position:new FormControl(''),
-      period:new FormControl('')
-  })
+  addexperiance() {
+    const newexperiance = new FormGroup({
+      workplace: new FormControl(''),
+      position: new FormControl(''),
+      period: new FormControl(''),
+    });
     this.workExperiance.push(newexperiance);
   }
-  deleteexperiance(i:number){
-    this.workExperiance.removeAt(i)
+  deleteexperiance(i: number) {
+    this.workExperiance.removeAt(i);
   }
 
   deleteSkill(i: number) {
     this.skills.removeAt(i);
   }
 
-  Submit(){
-    this.form.markAllAsTouched()
-    if(this.form.invalid) return
+  Submit() {
+    this.form.markAllAsTouched();
+    if (this.form.invalid) return;
     console.log(this.form.value);
 
     const {
@@ -115,47 +134,48 @@ activatedRoute=inject(ActivatedRoute)
       phone,
       address,
       skills,
-      workExperiance
-    } = this.form.value
+      workExperiance,
+    } = this.form.value;
 
-    if(id){
-      this.service.updateUser({
-        id,
-        firstName,
-      lastName,
-      birthDate,
-      phone,
-      address,
-      skills,
-      workExperiance
-      } as User)
-      .pipe(takeUntil(this.subj$))
-      .subscribe(()=>{
-        this.router.navigate(['/users'])
-      })
-    } else{
-      const randomid= Math.round(Math.random() * 100000)
+    if (id) {
+      this.service
+        .updateUser({
+          id,
+          firstName,
+          lastName,
+          birthDate,
+          phone,
+          address,
+          skills,
+          workExperiance,
+        } as User)
+        .pipe(takeUntil(this.subj$))
+        .subscribe(() => {
+          this.router.navigate(['/users']);
+        });
+    } else {
+      const randomid = Math.round(Math.random() * 100000);
       const user = {
-        id:String(randomid),
+        id: String(randomid),
         firstName,
         lastName,
         birthDate,
         phone,
         address,
         skills,
-        workExperiance
-      } as User
+        workExperiance,
+      } as User;
 
-      this.service.createUser(user)
-      .pipe(takeUntil(this.subj$))
-      .subscribe(()=>{
-      this.router.navigate(['/users'])
-      })
+      this.service
+        .createUser(user)
+        .pipe(takeUntil(this.subj$))
+        .subscribe(() => {
+          this.router.navigate(['/users']);
+        });
     }
   }
   ngOnDestroy(): void {
-    this.subj$.next('null')
-    this.subj$.complete()
+    this.subj$.next('null');
+    this.subj$.complete();
   }
-
 }
